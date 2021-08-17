92be4e6fbae0f4c49be463337b52ef24986e5470
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-08-17 20:04:42 +0300

    added test

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,5 +1,12 @@
-from web_scraper import __version__
+from web_scraper.scraper import *
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_get_citations_needed_count():
+    expected=5
+    actual=get_citations_needed_count(requests_url)
+    assert expected==actual
+
+
+def test_get_citations_needed_report():
+    assert get_citations_needed_report(requests_url)           
+    
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -6,7 +6,24 @@ requests_url = "https://en.wikipedia.org/wiki/History_of_Mexico"
 def get_citations_needed_count(url):
     response = requests.get(url)
     soup=BeautifulSoup(response.text,'html.parser')
-    result=soup.find_all('sup',class_ = 'inline-Template')
+    result=soup.find_all(class_ = 'Inline-Template')
     return(len(result))
 
 # get_citations_needed_count(requests_url)
+
+def get_citations_needed_report(url):
+    response = requests.get(url)
+    soup=BeautifulSoup(response.text,'html.parser')
+    result=soup.find_all(class_ = 'Inline-Template' )
+    paragraph_contain_citation = []
+
+    for p in result:
+        paragraph_contain_citation.append(p.parent.text.strip())
+    return '\n'.join(paragraph_contain_citation)
+
+
+
+if __name__=='__main__':
+
+    print(get_citations_needed_report(requests_url))
+    print(get_citations_needed_count(requests_url))
